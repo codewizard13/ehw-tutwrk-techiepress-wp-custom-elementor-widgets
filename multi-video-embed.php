f476fbcd9226cbcd72808c340046095bba0b34c1
Eric L. Hepperle <codewizard13@gmail.com>
2023-12-19 10:46:42 -0600

    INITIAL COMMIT:  Plugin - Multi Video Embed Widget

---
+++ multi-video-embed.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Plugin Name: Multi Video Embed
+ * Plugin URI:
+ * Author: Eric Hepperle
+ * Author URI: EricHepperleDesigns.com
+ * Description: Custom Elementor widget plugin to render videos and livestreams from various sources and formats (Rumble, Facebook, YouTube, Vimeo, etc.)
+ * Version: 0.1.0
+ * License: 
+ * License URL:
+ * Text Domain: ehw-multi-video-embed 
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+final class MultiVideoEmbed {
+
+  const VERSION = '0.1.0';
+  const ELEMENTOR_MINIMUM_VERSION = '3.0.0';
+  const PHP_MINIMUM_VERSION = '7.4';
+
+  private static $_instance = null;
+
+
+
+
+  public static function get_instance() {
+    if ( null == self::$_instance ) {
+      self::$_instance = new Self();
+    }
+
+    return self::$_instance;
+  }
+
+}
