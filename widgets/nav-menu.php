98762d88879d42f6c2ebe5044937b02773c400c9
Eric L. Hepperle <codewizard13@gmail.com>
2023-12-19 15:02:05 -0600

    UPDATE nav-menu.php:  RETURN 'EHW Menu' from get_name()

--- widgets/nav-menu.php
+++ widgets/nav-menu.php
@@ -1,10 +1,15 @@
 <?php
 
 use Elementor\Widget_Base;
+
 /**
  * Have the widget code for the Custom Elementor Nav Menu
  */
 
  class Nav_Menu extends Widget_Base {
+
+  public function get_name() {
+    return 'EHW Menu';
+  }
 
  }
