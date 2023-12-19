42006eb44a8819cfed5b2f2d58097e45f8e80fc1
Eric L. Hepperle <codewizard13@gmail.com>
2023-12-19 16:13:19 -0600

    UPDATE nav-menu.php:  ADD widget to favorites category

--- widgets/nav-menu.php
+++ widgets/nav-menu.php
@@ -25,7 +25,7 @@ use Elementor\Widget_Base;
 
   public function get_categories() {
     // TODO: Add our own category in Elementor
-    return ['basic'];
+    return ['basic', 'favorites'];
   }
 
   public function _register_control() {
