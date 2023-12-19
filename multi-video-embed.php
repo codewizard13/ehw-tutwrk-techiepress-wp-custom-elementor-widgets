6e5554b2cca9c559e5a285e59e88f7a7000bccbd
Eric L. Hepperle <codewizard13@gmail.com>
2023-12-19 13:47:05 -0600

    UPDATE multi-video-embed.php:  CALL echo_header_2() with ehw_do_action hook

--- multi-video-embed.php
+++ multi-video-embed.php
@@ -61,3 +61,10 @@ final class MultiVideoEmbed {
 }
 
 // MultiVideoEmbed::get_instance();
+
+add_action( 'ehw_do_action', 'echo_header_2' );
+
+
+function echo_header_2() {
+  echo '<h2>This is our header two</h2>';
+}
